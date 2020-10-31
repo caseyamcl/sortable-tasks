602512ec8ec22e62435966f4dd6b12b247e69a4c
Casey McLaughlin <caseyamcl@gmail.com>
2020-10-31 15:44:31 -0400

    Added headers to all files.

--- src/SortableTasksIterator.php
+++ src/SortableTasksIterator.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * Sortable Tasks Library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/caseyamcl/sortable-tasks
+ * @version 1
+ * @package caseyamcl/sortable-tasks
+ * @author Casey McLaughlin <me@caseymclaughlin.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * ------------------------------------------------------------------
+ */
+
 declare(strict_types=1);
 
 namespace SortableTasks;
