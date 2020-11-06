b6eb66a1b98e942fd435482667149b34d574907e
Casey McLaughlin <caseyamcl@gmail.com>
2020-11-06 14:47:08 -0500

    feat: Added TwoWaySorter.php for sorting strings

---
+++ src/TwoWaySorter.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * Sortable Tasks Library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/caseyamcl/sortable-tasks
+ * @version 1
+ * @package caseyamcl/sortable-tasks
+ * @author Casey McLaughlin <me@caseymclaughlin.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * ------------------------------------------------------------------
+ */
+
+declare(strict_types=1);
+
+namespace SortableTasks;
+
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
+use MJS\TopSort\CircularDependencyException;
+use MJS\TopSort\ElementNotFoundException;
+use MJS\TopSort\Implementations\StringSort;
+use MJS\TopSort\TopSortInterface;
+
+/**
+ * Two-way sorter contains basic logic to support sorting with MJS\TopSort
+ *
+ * @author Casey McLaughlin <me@caseymclaughlin.com>
+ */
+class TwoWaySorter implements IteratorAggregate, Countable
+{
+    private ?TopSortInterface $sorter;
+
+    /**
+     * @var array|string[]
+     */
+    private array $tasks = [];
+
+    /**
+     * @var array|string[]
+     */
+    private array $extraDependencies = [];
+
+    /**
+     * TwoWaySorter constructor.
+     * @param TopSortInterface|null $sorter
+     */
+    public function __construct(?TopSortInterface $sorter = null)
+    {
+        $this->sorter = $sorter ?: new StringSort();
+    }
+
+    /**
+     * Add item and its dependencies
+     *
+     * @param string $task
+     * @param array|string[] $dependencies
+     * @param array|string[] $mustRunBefore
+     */
+    public function add(string $task, array $dependencies = [], array $mustRunBefore = []): void
+    {
+        $this->tasks[$task] = $dependencies;
+
+        if (!empty($mustRunBefore)) {
+            foreach ($mustRunBefore as $depName) {
+                $this->extraDependencies[$depName][] = $task;
+            }
+        }
+    }
+
+    /**
+     * Sort items and return iterator
+     *
+     * @return ArrayIterator
+     * @throws ElementNotFoundException
+     * @throws CircularDependencyException
+     */
+    public function getIterator(): ArrayIterator
+    {
+        if (count($this->tasks) === 0) {
+            return new ArrayIterator([]);
+        }
+
+        $sorter = clone $this->sorter;
+
+        foreach ($this->tasks as $item => $dependencies) {
+            // fancy logic here...
+            if (isset($this->extraDependencies[$item])) {
+                $dependencies = array_merge($dependencies, $this->extraDependencies[$item]);
+            }
+
+            $sorter->add($item, $dependencies);
+        }
+
+        return new ArrayIterator($sorter->sort());
+    }
+
+    /**
+     * Alias for self::getIterator()
+     *
+     * @return ArrayIterator
+     * @throws CircularDependencyException
+     * @throws ElementNotFoundException
+     */
+    public function sort(): ArrayIterator
+    {
+        return $this->getIterator();
+    }
+
+    /**
+     * Count items
+     *
+     * @return int
+     */
+    public function count(): int
+    {
+        return count($this->tasks);
+    }
+}
---
+++ tests/TwoWaySorterTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Sortable Tasks Library
+ *
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/caseyamcl/sortable-tasks
+ * @version 1
+ * @package caseyamcl/sortable-tasks
+ * @author Casey McLaughlin <me@caseymclaughlin.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * ------------------------------------------------------------------
+ */
+
+declare(strict_types=1);
+
+namespace SortableTasks;
+
+use PHPUnit\Framework\TestCase;
+
+class TwoWaySorterTest extends TestCase
+{
+    public function testInstantiate(): void
+    {
+        $sorter = new TwoWaySorter();
+        $this->assertInstanceOf(TwoWaySorter::class, $sorter);
+    }
+
+    public function testBasicDependencies(): void
+    {
+        $sorter = new TwoWaySorter();
+        $sorter->add('a', ['b', 'c']);
+        $sorter->add('b', ['c']);
+        $sorter->add('c');
+        $this->assertSame(['c', 'b', 'a'], $sorter->sort()->getArrayCopy());
+    }
+
+    public function testTwoWayDependency(): void
+    {
+        $sorter = new TwoWaySorter();
+        $sorter->add('a', ['b', 'c', 'd'], ['e']);
+        $sorter->add('b', ['c'], ['d']);
+        $sorter->add('c');
+        $sorter->add('d');
+        $sorter->add('e');
+
+        $this->assertSame(['c', 'b', 'd', 'a', 'e'], $sorter->sort()->getArrayCopy());
+    }
+
+    public function testCountWhenItemsPresent(): void
+    {
+        $sorter = new TwoWaySorter();
+        $sorter->add('a', ['b']);
+        $sorter->add('b');
+        $this->assertSame(2, $sorter->count());
+    }
+
+    public function testCountWhenEmpty(): void
+    {
+        $sorter = new TwoWaySorter();
+        $this->assertSame(0, $sorter->count());
+    }
+}
